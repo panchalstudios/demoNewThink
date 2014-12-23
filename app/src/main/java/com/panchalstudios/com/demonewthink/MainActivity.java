0904a6ae2bbc8399a954b88c70aae23ddcc622b4
panchalstudios <ravi-panchal@users.noreply.github.com>
2014-12-22 22:59:45 -0500

    Layout demo

--- app/src/main/java/com/panchalstudios/com/demonewthink/MainActivity.java
+++ app/src/main/java/com/panchalstudios/com/demonewthink/MainActivity.java
@@ -1,17 +1,33 @@
 package com.panchalstudios.com.demonewthink;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-// we are starting
+        import android.os.Bundle;
+        import android.support.v7.app.ActionBarActivity;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+
 public class MainActivity extends ActionBarActivity {
+
+    // 17. Create the components here that you want to use in your code
+
+    private Button answerYesButton, answerNoButton;
+    private EditText usersNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 18. Initialize all of your components in the code here
+
+        answerYesButton = (Button) findViewById(R.id.answer_yes_button);
+        answerNoButton = (Button) findViewById(R.id.answer_no_button);
+        usersNameEditText = (EditText) findViewById(R.id.users_name_edit_text);
     }
 
 
@@ -28,12 +44,29 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onYesButtonClick(View view) {
+
+        String usersName = String.valueOf(usersNameEditText.getText());
+
+        String yourYesResponse = "That is great " + usersName;
+
+        // 19. We display a short popup or Toast when the user clicks
+        // LENGTH_SHORT or LENGTH_LONG defines how long the toast shows
+
+        Toast.makeText(this, yourYesResponse, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onNoButtonClick(View view) {
+
+        String usersName = String.valueOf(usersNameEditText.getText());
+        String yourNoResponse = "To bad " + usersName;
+        Toast.makeText(this, yourNoResponse, Toast.LENGTH_SHORT).show();
+
     }
 }
